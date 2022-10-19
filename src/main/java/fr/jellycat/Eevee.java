ed8cddd73a2bceffce4603d7a93cc637c8b9c893
salty-max <max@jellycat.fr>
2022-10-20 01:04:15 +0200

    first commit

---
+++ src/main/java/fr/jellycat/Eevee.java
@@ -0,0 +1,10 @@
+package fr.jellycat;
+
+import java.io.IOException;
+
+public final class Eevee {
+
+    public static void main(String[] args) throws IOException {
+        System.out.print("Hello World!");
+    }
+}
---
+++ src/test/java/fr/jellycat/EeveeTest.java
@@ -0,0 +1,18 @@
+package fr.jellycat;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class EeveeTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+}
