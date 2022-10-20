4a946831b3be5bad86018017b890f272e4b7fef7
salty-max <max@jellycat.fr>
2022-10-20 03:35:41 +0200

    allows to print tokens from file or prompt

--- src/main/java/fr/jellycat/Eevee.java
+++ src/main/java/fr/jellycat/Eevee.java
@@ -1,10 +1,67 @@
 package fr.jellycat;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 public final class Eevee {
+    static boolean hadError = false;
 
     public static void main(String[] args) throws IOException {
-        System.out.print("Hello World!");
+        if (args.length > 1) {
+            System.out.print("Usage: jeevee [script]");
+            System.exit(64);
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+
+        if (hadError)
+            System.exit(65);
+    }
+
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for (;;) {
+            System.out.print("> ");
+            String line = reader.readLine();
+
+            if (line == null)
+                break;
+
+            run(line);
+            hadError = false;
+        }
+    }
+
+    private static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        // For now, just print the tokens.
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
+    }
+
+    static void error(int line, int column, String message) {
+        report(line, column, "", message);
+    }
+
+    private static void report(int line, int column, String where, String message) {
+        System.err.println("Error" + where + " (" + line + ":" + column + "): " + message);
+        hadError = true;
     }
 }
---
+++ src/main/java/fr/jellycat/Token.java
@@ -0,0 +1,21 @@
+package fr.jellycat;
+
+class Token {
+    final TokenType type;
+    final String lexeme;
+    final Object literal;
+    final int line;
+    final int column;
+
+    Token(TokenType type, String lexeme, Object literal, int line, int column) {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.literal = literal;
+        this.line = line;
+        this.column = column;
+    }
+
+    public String toString() {
+        return type + " " + lexeme + " " + literal;
+    }
+}
