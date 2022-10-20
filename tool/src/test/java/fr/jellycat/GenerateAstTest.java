6f8d53c78415af63649b5fae80310e5b0615d835
salty-max <max@jellycat.fr>
2022-10-20 18:20:01 +0200

    creates simple AST generator

---
+++ tool/src/test/java/fr/jellycat/GenerateAstTest.java
@@ -0,0 +1,18 @@
+package fr.jellycat;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class GenerateAstTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
